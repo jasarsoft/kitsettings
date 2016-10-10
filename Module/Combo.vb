3d462eba0838a5bd63c664ec415fcba1bb16efe8
jasarsoft <edinjasar14@gmail.com>
2016-10-10 18:59:24 +0200

    new module
    add new Combo module

---
+++ Module/Combo.vb
@@ -0,0 +1,25 @@
+﻿Public Module Combo
+
+    Public Sub SetBoolValue(ByRef comboBox As ComboBox, ByVal inputValue As Boolean)
+
+        If inputValue Then
+            comboBox.SelectedIndex = 1
+        Else
+            comboBox.SelectedIndex = 0
+        End If
+
+    End Sub
+
+    Public Function GetBoolValue(ByRef comboBox As ComboBox) As Boolean
+
+        If comboBox.SelectedIndex = 0 Then
+            Return False
+        ElseIf comboBox.SelectedIndex = 1 Then
+            Return True
+        Else
+            Return Nothing
+        End If
+
+    End Function
+
+End Module
