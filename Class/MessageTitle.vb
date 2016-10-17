1e89ac8cc80ac9f62adc24f0b892d7c9a1a0a350
jasarsoft <edinjasar14@gmail.com>
2016-10-17 17:47:01 +0200

    edited and additional comments

--- Class/MessageTitle.vb
+++ Class/MessageTitle.vb
@@ -1,28 +1,54 @@
-﻿Public NotInheritable Class MessageTitle
+﻿
+''' <summary>
+''' Class title for message
+''' </summary>
+Public NotInheritable Class MessageTitle
 
+#Region "Filed"
+    ''' <summary>
+    ''' Constants title
+    ''' </summary>
     Private Structure Title
         Private Const appTitle As String = "Kitserver 6 Settings"
         Public Const titleInfo As String = appTitle & " | Information"
         Public Const titleError As String = appTitle & " | Error"
         Public Const titleWarning As String = appTitle & " | Warning"
     End Structure
+#End Region
 
-    Public ReadOnly Property TitleInfo
+#Region "Properties"
+    ''' <summary>
+    ''' Get title for information message
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>Kitserver 6 Settings | Information</returns>
+    Public ReadOnly Property TitleInfo As String
         Get
             Return Title.titleInfo
         End Get
     End Property
 
-    Public ReadOnly Property TitleError
+    ''' <summary>
+    ''' Get title for error message
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>Kitserver 6 Settings | Error</returns>
+    Public ReadOnly Property TitleError As String
         Get
             Return Title.titleError
         End Get
     End Property
 
-    Public ReadOnly Property TitleWarning
+    ''' <summary>
+    ''' Get title for warning message
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>Kitserver 6 Settings | Warning</returns>
+    Public ReadOnly Property TitleWarning As String
         Get
             Return Title.titleWarning
         End Get
     End Property
+#End Region
 
 End Class
