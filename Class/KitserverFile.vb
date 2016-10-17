4a014c2e442333e299eec1cc74e70ab09e74eb93
jasarsoft <edinjasar14@gmail.com>
2016-10-17 20:19:40 +0200

    additional comments

--- Class/KitserverFile.vb
+++ Class/KitserverFile.vb
@@ -1,31 +1,63 @@
 ﻿Imports System.IO
 
-Public Class KitserverFile
+''' <summary>
+''' Kitserver 6 File Class
+''' </summary>
+Public NotInheritable Class KitserverFile
 
+#Region "Field"
+    ''' <summary>
+    ''' Name of the application
+    ''' </summary>
     Private Structure AppFile
         Public Const keyBind As String = "keybind.exe"
         Public Const lodMixer As String = "lodcfg.exe"
         Public Const setup As String = "setup.exe"
     End Structure
+#End Region
 
+#Region "Properties"
+    ''' <summary>
+    ''' Name KeyBind application
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>keybind.exe</returns>
     Public ReadOnly Property AppKeyBind As String
         Get
             Return AppFile.keyBind
         End Get
     End Property
 
+    ''' <summary>
+    ''' Name LodMixer application
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>lodcfg.exe</returns>
     Public ReadOnly Property AppLodMixer As String
         Get
             Return AppFile.lodMixer
         End Get
     End Property
 
+    ''' <summary>
+    ''' Name Setup application
+    ''' </summary>
+    ''' <value>String</value>
+    ''' <returns>setup.exe</returns>
     Public ReadOnly Property AppSetup As String
         Get
             Return AppFile.setup
         End Get
     End Property
+#End Region
 
+#Region "Methods"
+    ''' <summary>
+    ''' Checking existence of the input file
+    ''' </summary>
+    ''' <param name="fileName"></param>
+    ''' <returns>Boolean</returns>
+    ''' <remarks>Sends a message if the file does not exist</remarks>
     Public Function Check(ByVal fileName As String) As Boolean
         If File.Exists(fileName) Then
             Return True
@@ -36,6 +68,10 @@ Public Class KitserverFile
         Return False
     End Function
 
+    ''' <summary>
+    ''' Display error message
+    ''' </summary>
+    ''' <param name="fileName"></param>
     Private Sub MessageError(ByVal fileName As String)
         Dim msgText As String
         Dim msgTitle As New MessageTitle()
@@ -45,5 +81,6 @@ Public Class KitserverFile
 
         MessageBox.Show(msgText, msgTitle.TitleWarning, MessageBoxButtons.OK, MessageBoxIcon.Warning)
     End Sub
+#End Region
 
 End Class
