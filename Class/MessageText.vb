74acc8e52d0c8cd1f152ea185d8c0734ad036455
jasarsoft <edinjasar14@gmail.com>
2016-10-19 00:21:38 +0200

    additional comments

--- Class/MessageText.vb
+++ Class/MessageText.vb
@@ -1,5 +1,13 @@
-﻿Public NotInheritable Class MessageText
+﻿
+''' <summary>
+''' General class for text message
+''' </summary>
+Public NotInheritable Class MessageText
 
+    ''' <summary>
+    ''' Fields for content of the message
+    ''' </summary>
+#Region "Fileds"
     Private _id00 As String
     Private _id01 As String
     Private _id02 As String
@@ -11,7 +19,108 @@
     Private _id14 As String
     Private _id15 As String
     Private _id16 As String
+#End Region
 
+#Region "Properties"
+    ''' <summary>
+    ''' Message suggestions to check administrative privileges
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID00 As String
+        Get
+            Return _id00
+        End Get
+    End Property
+    ''' <summary>
+    ''' Message questions to correct the file
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property ID01 As String
+        Get
+            Return _id01
+        End Get
+    End Property
+    ''' <summary>
+    ''' Message questions to create a new file
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID02 As String
+        Get
+            Return _id02
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' The message that a file contained an invalid parameter
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID10 As String
+        Get
+            Return _id10
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message that a file can not be writen
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID11 As String
+        Get
+            Return _id11
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message that a file can not exist
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID12 As String
+        Get
+            Return _id12
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message that a file can not be deleted
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID13 As String
+        Get
+            Return _id13
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message that a file can not be modified
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID14 As String
+        Get
+            Return _id14
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message that a file can not be created
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID15 As String
+        Get
+            Return _id15
+        End Get
+    End Property
+    ''' <summary>
+    ''' The message on successful creation
+    ''' </summary>
+    ''' <value>String</value>
+    Public ReadOnly Property ID16 As String
+        Get
+            Return _id16
+        End Get
+    End Property
+#End Region
+
+#Region "Constructor"
+    ''' <summary>
+    ''' Default initialization constructor
+    ''' </summary>
     Public Sub New()
         _id00 = "Check administrative privileges to application" & Environment.NewLine
         _id00 += "or use the file from third-party software."
@@ -30,66 +139,6 @@
         _id15 = " can not be created."
         _id16 = " was successfully created" & Environment.NewLine & "with an inital recommended settings."
     End Sub
+#End Region
 
-    Public ReadOnly Property ID00 As String
-        Get
-            Return _id00
-        End Get
-    End Property
-
-    Public ReadOnly Property ID01 As String
-        Get
-            Return _id01
-        End Get
-    End Property
-
-    Public ReadOnly Property ID02 As String
-        Get
-            Return _id02
-        End Get
-    End Property
-
-
-    Public ReadOnly Property ID10 As String
-        Get
-            Return _id10
-        End Get
-    End Property
-
-    Public ReadOnly Property ID11 As String
-        Get
-            Return _id11
-        End Get
-    End Property
-
-    Public ReadOnly Property ID12 As String
-        Get
-            Return _id12
-        End Get
-    End Property
-
-    Public ReadOnly Property ID13 As String
-        Get
-            Return _id13
-        End Get
-    End Property
-
-    Public ReadOnly Property ID14 As String
-        Get
-            Return _id14
-        End Get
-    End Property
-
-    Public ReadOnly Property ID15 As String
-        Get
-            Return _id15
-        End Get
-    End Property
-
-    Public ReadOnly Property ID16 As String
-        Get
-            Return _id16
-        End Get
-    End Property
-    
 End Class
