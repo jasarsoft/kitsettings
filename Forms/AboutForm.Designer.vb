bbe203c7ef4da0b1d7580d086091b83c1d7b418d
jasarsoft <edinjasar14@gmail.com>
2017-02-01 04:03:17 +0100

    rename about form

---
+++ Forms/AboutForm.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonClose = New System.Windows.Forms.Button()
+        Me.labelVersion = New System.Windows.Forms.Label()
+        Me.labelName = New System.Windows.Forms.Label()
+        Me.labelDeveloper = New System.Windows.Forms.Label()
+        Me.labelWebsite = New System.Windows.Forms.Label()
+        Me.labelDescription = New System.Windows.Forms.Label()
+        Me.labelDescriptionData = New System.Windows.Forms.Label()
+        Me.labelWebsiteData = New System.Windows.Forms.Label()
+        Me.labelDeveloperData = New System.Windows.Forms.Label()
+        Me.labelVersionData = New System.Windows.Forms.Label()
+        Me.labelNameData = New System.Windows.Forms.Label()
+        Me.lineHorizontal = New System.Windows.Forms.Label()
+        Me.labelLicenseData = New System.Windows.Forms.Label()
+        Me.labelLicense = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'buttonClose
+        '
+        Me.buttonClose.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.buttonClose.Location = New System.Drawing.Point(12, 215)
+        Me.buttonClose.Name = "buttonClose"
+        Me.buttonClose.Size = New System.Drawing.Size(380, 40)
+        Me.buttonClose.TabIndex = 0
+        Me.buttonClose.Text = "CLOSE ABOUT"
+        Me.buttonClose.UseVisualStyleBackColor = True
+        '
+        'labelVersion
+        '
+        Me.labelVersion.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelVersion.Location = New System.Drawing.Point(12, 47)
+        Me.labelVersion.Name = "labelVersion"
+        Me.labelVersion.Size = New System.Drawing.Size(85, 20)
+        Me.labelVersion.TabIndex = 2
+        Me.labelVersion.Text = "Version:"
+        Me.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelName
+        '
+        Me.labelName.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelName.Location = New System.Drawing.Point(12, 21)
+        Me.labelName.Name = "labelName"
+        Me.labelName.Size = New System.Drawing.Size(85, 20)
+        Me.labelName.TabIndex = 1
+        Me.labelName.Text = "Name:"
+        Me.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelDeveloper
+        '
+        Me.labelDeveloper.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelDeveloper.Location = New System.Drawing.Point(12, 73)
+        Me.labelDeveloper.Name = "labelDeveloper"
+        Me.labelDeveloper.Size = New System.Drawing.Size(85, 20)
+        Me.labelDeveloper.TabIndex = 3
+        Me.labelDeveloper.Text = "Developer:"
+        Me.labelDeveloper.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelWebsite
+        '
+        Me.labelWebsite.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelWebsite.Location = New System.Drawing.Point(12, 125)
+        Me.labelWebsite.Name = "labelWebsite"
+        Me.labelWebsite.Size = New System.Drawing.Size(85, 20)
+        Me.labelWebsite.TabIndex = 5
+        Me.labelWebsite.Text = "Website:"
+        Me.labelWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelDescription
+        '
+        Me.labelDescription.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelDescription.Location = New System.Drawing.Point(12, 151)
+        Me.labelDescription.Name = "labelDescription"
+        Me.labelDescription.Size = New System.Drawing.Size(85, 20)
+        Me.labelDescription.TabIndex = 6
+        Me.labelDescription.Text = "Description:"
+        Me.labelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelDescriptionData
+        '
+        Me.labelDescriptionData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelDescriptionData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelDescriptionData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelDescriptionData.Location = New System.Drawing.Point(112, 151)
+        Me.labelDescriptionData.Name = "labelDescriptionData"
+        Me.labelDescriptionData.Size = New System.Drawing.Size(280, 50)
+        Me.labelDescriptionData.TabIndex = 12
+        Me.labelDescriptionData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelWebsiteData
+        '
+        Me.labelWebsiteData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelWebsiteData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelWebsiteData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelWebsiteData.Location = New System.Drawing.Point(112, 125)
+        Me.labelWebsiteData.Name = "labelWebsiteData"
+        Me.labelWebsiteData.Size = New System.Drawing.Size(280, 20)
+        Me.labelWebsiteData.TabIndex = 11
+        Me.labelWebsiteData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelDeveloperData
+        '
+        Me.labelDeveloperData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelDeveloperData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelDeveloperData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelDeveloperData.Location = New System.Drawing.Point(112, 73)
+        Me.labelDeveloperData.Name = "labelDeveloperData"
+        Me.labelDeveloperData.Size = New System.Drawing.Size(280, 20)
+        Me.labelDeveloperData.TabIndex = 9
+        Me.labelDeveloperData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelVersionData
+        '
+        Me.labelVersionData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelVersionData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelVersionData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelVersionData.Location = New System.Drawing.Point(112, 47)
+        Me.labelVersionData.Name = "labelVersionData"
+        Me.labelVersionData.Size = New System.Drawing.Size(280, 20)
+        Me.labelVersionData.TabIndex = 8
+        Me.labelVersionData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelNameData
+        '
+        Me.labelNameData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelNameData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelNameData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelNameData.Location = New System.Drawing.Point(112, 21)
+        Me.labelNameData.Name = "labelNameData"
+        Me.labelNameData.Size = New System.Drawing.Size(280, 20)
+        Me.labelNameData.TabIndex = 7
+        Me.labelNameData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lineHorizontal
+        '
+        Me.lineHorizontal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lineHorizontal.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lineHorizontal.Location = New System.Drawing.Point(12, 210)
+        Me.lineHorizontal.Name = "lineHorizontal"
+        Me.lineHorizontal.Size = New System.Drawing.Size(380, 2)
+        Me.lineHorizontal.TabIndex = 13
+        Me.lineHorizontal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelLicenseData
+        '
+        Me.labelLicenseData.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.labelLicenseData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelLicenseData.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelLicenseData.Location = New System.Drawing.Point(112, 99)
+        Me.labelLicenseData.Name = "labelLicenseData"
+        Me.labelLicenseData.Size = New System.Drawing.Size(280, 20)
+        Me.labelLicenseData.TabIndex = 10
+        Me.labelLicenseData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelLicense
+        '
+        Me.labelLicense.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.labelLicense.Location = New System.Drawing.Point(12, 99)
+        Me.labelLicense.Name = "labelLicense"
+        Me.labelLicense.Size = New System.Drawing.Size(85, 20)
+        Me.labelLicense.TabIndex = 4
+        Me.labelLicense.Text = "License:"
+        Me.labelLicense.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'FormAbout
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(404, 267)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.labelLicenseData)
+        Me.Controls.Add(Me.labelLicense)
+        Me.Controls.Add(Me.lineHorizontal)
+        Me.Controls.Add(Me.labelNameData)
+        Me.Controls.Add(Me.labelVersionData)
+        Me.Controls.Add(Me.labelDeveloperData)
+        Me.Controls.Add(Me.labelWebsiteData)
+        Me.Controls.Add(Me.labelDescriptionData)
+        Me.Controls.Add(Me.labelDescription)
+        Me.Controls.Add(Me.labelWebsite)
+        Me.Controls.Add(Me.labelDeveloper)
+        Me.Controls.Add(Me.labelName)
+        Me.Controls.Add(Me.labelVersion)
+        Me.Controls.Add(Me.buttonClose)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Location = New System.Drawing.Point(256, 128)
+        Me.MaximizeBox = False
+        Me.Name = "FormAbout"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Kitserver 6 Settings | About"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents buttonClose As System.Windows.Forms.Button
+    Friend WithEvents labelVersion As System.Windows.Forms.Label
+    Friend WithEvents labelName As System.Windows.Forms.Label
+    Friend WithEvents labelDeveloper As System.Windows.Forms.Label
+    Friend WithEvents labelWebsite As System.Windows.Forms.Label
+    Friend WithEvents labelDescription As System.Windows.Forms.Label
+    Friend WithEvents labelDescriptionData As System.Windows.Forms.Label
+    Friend WithEvents labelWebsiteData As System.Windows.Forms.Label
+    Friend WithEvents labelDeveloperData As System.Windows.Forms.Label
+    Friend WithEvents labelVersionData As System.Windows.Forms.Label
+    Friend WithEvents labelNameData As System.Windows.Forms.Label
+    Private WithEvents lineHorizontal As System.Windows.Forms.Label
+    Friend WithEvents labelLicenseData As System.Windows.Forms.Label
+    Friend WithEvents labelLicense As System.Windows.Forms.Label
+End Class
