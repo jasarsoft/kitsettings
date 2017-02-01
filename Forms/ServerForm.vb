a2fe16c12a8d988e5d6906212897bcf1e4b15408
jasarsoft <edinjasar14@gmail.com>
2017-02-01 04:11:30 +0100

    server form edit
    - new close button
    - new event click
    - settings form as dialog

---
+++ Forms/ServerForm.vb
@@ -0,0 +1,18 @@
+﻿Public Class ServerForm
+
+    Public Sub New()
+        ' Designer components
+        InitializeComponent()
+    End Sub
+
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles buttonSave.Click
+        'Me.DialogResult = Windows.Forms.DialogResult.OK 'setting in design mode
+        Me.Close()
+    End Sub
+
+    Private Sub ButtonClose_Click(sender As Object, e As EventArgs) Handles ButtonClose.Click
+        'Me.DialogResult = Windows.Forms.DialogResult.Cancel 'setting in design mode
+        Me.Close()
+    End Sub
+End Class
