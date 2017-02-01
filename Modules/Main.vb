1ec84b7e2d4e1a6b390601610039d8529e754987
jasarsoft <edinjasar14@gmail.com>
2017-02-01 04:16:13 +0100

    interaciton form
    - delete namespace for form
    - change relationship form

--- Modules/Main.vb
+++ Modules/Main.vb
@@ -1,30 +1,45 @@
-﻿
-Namespace Modules
-    Public Module Main
-        Private _formMain As Forms.Main
+﻿Public Module Main
+    'Private _host As HostForm
+    'Private _server As ServerForm
+    Private _settings As SettingsForm
 
 
-        Sub New()
-            _formMain = New Forms.Main()
-        End Sub
+    Sub New()
+        _settings = New SettingsForm()
+    End Sub
 
-        Public Property Form As Forms.Main
-            Get
-                Return _formMain
-            End Get
-            Set(value As Forms.Main)
-                _formMain = value
-            End Set
-        End Property
+    'Public Property Settings As SettingsForm
+    '    Get
+    '        Return _settings
+    '    End Get
+    '    Set(value As SettingsForm)
+    '        _settings = value
+    '    End Set
+    'End Property
+
+    'Public Property Host As HostForm
+    '    Get
+    '        Return _host
+    '    End Get
+    '    Set(value As HostForm)
+    '        _host = value
+    '    End Set
+    'End Property
+
+    'Public Property Server As ServerForm
+    '    Get
+    '        Return _server
+    '    End Get
+    '    Set(value As ServerForm)
+    '        _server = value
+    '    End Set
+    'End Property
 
 
-        <STAThread> _
-        Public Sub Main()
-            Application.EnableVisualStyles()
-            Application.Run(_formMain)
-        End Sub
+    <STAThread> _
+    Public Sub Main()
+        Application.EnableVisualStyles()
+        Application.Run(_settings)
+    End Sub
 
-    End Module
-End Namespace
-
-
+End Module
