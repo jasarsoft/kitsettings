f18e254ef3d6b1e4034b51feb1d8d2e5233edd50
jasarsoft <edinjasar14@gmail.com>
2017-02-06 01:56:32 +0100

    edit read/write method

--- Classes/Host.vb
+++ Classes/Host.vb
@@ -44,13 +44,16 @@ Namespace Classes
             Dim line As String
 
             If Me.Check() Then
+                Dim header As String = "#Kitserver 6 Settings"
+                Dim footer As String = "#####################"
+
                 Using sr As StreamReader = New StreamReader(_path)
                     Do
                         line = sr.ReadLine
-                        If line.StartsWith("#Kitserver 6 Settings") Then
+                        If line.StartsWith(header) Then
                             Do
                                 line = sr.ReadLine
-                                If line.StartsWith("#####################") Then
+                                If line.StartsWith(footer) Then
                                     Return True
                                 End If
                             Loop Until sr.EndOfStream
@@ -67,35 +70,27 @@ Namespace Classes
 
             _servers.Clear()
             If Me.Check() Then
+                Dim header As String = "#Kitserver 6 Settings"
+                Dim footer As String = "#####################"
+
                 Using sr As StreamReader = New StreamReader(_path)
                     Do
                         line = sr.ReadLine
-                        If line.StartsWith("#Kitserver 6 Settings") Then
+                        If line.StartsWith(header) Then
                             Do
                                 line = sr.ReadLine
-                                If line.StartsWith("#####################") Then
+                                If line.StartsWith(footer) Then
                                     Return True
                                 Else
                                     _servers.Add(line)
                                 End If
                             Loop Until sr.EndOfStream
-
-                            'While True
-                            '    line = sr.ReadLine
-                            '    If line.StartsWith("#####################") Then
-                            '        Return True
-                            '    Else
-                            '        _servers.Add(line)
-                            '    End If
-                            '    If sr.EndOfStream Then
-                            '        Return False
-                            '    End If
-                            'End While
                         End If
                     Loop Until sr.EndOfStream
                 End Using
             End If
 
+            _servers.Clear()
             Return False
         End Function
 
@@ -104,7 +99,7 @@ Namespace Classes
             Dim done As Boolean = False
             Dim text As New List(Of String)
 
-            If File.Exists(_path) Then
+            If Me.Check() Then
                 Dim header As String = "#Kitserver 6 Settings"
                 Dim footer As String = "#####################"
 
@@ -113,7 +108,7 @@ Namespace Classes
                         line = sr.ReadLine
                         If line.StartsWith(header) Then
                             text.Add(line)
-                            While True
+                            Do
                                 line = sr.ReadLine
                                 If line.StartsWith(footer) Then
                                     For Each name As String In _servers
@@ -121,10 +116,20 @@ Namespace Classes
                                     Next
                                     text.Add(line)
                                     done = True
-                                    Exit While
                                 End If
-                                If sr.EndOfStream Then Exit While
-                            End While
+                            Loop Until done Or sr.EndOfStream
+                            'While True
+                            '    line = sr.ReadLine
+                            '    If line.StartsWith(footer) Then
+                            '        For Each name As String In _servers
+                            '            text.Add(name)
+                            '        Next
+                            '        text.Add(line)
+                            '        done = True
+                            '        Exit While
+                            '    End If
+                            '    If sr.EndOfStream Then Exit While
+                            'End While
                         Else
                             text.Add(line)
                         End If
@@ -137,14 +142,17 @@ Namespace Classes
                             text.Add(name)
                         Next
                         text.Add(footer)
+                        done = True
                     End If
                 End Using
 
+                If Not done Then Return done
                 Using sw As StreamWriter = File.CreateText(_path)
                     For Each name As String In text
                         sw.WriteLine(name)
                     Next
-                End Using
+                    done = True
+                End Using                
             End If
 
             Return done
