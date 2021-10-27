66b5bfbe849d1aa5166ced31eb8ede30d73b72de
Edin Jašarević <edinjasar14@gmail.com>
2021-10-27 20:35:11 +0200

    add parameter class

---
+++ Parameter.vb
@@ -0,0 +1,11 @@
+﻿Public Module Parameter
+
+    Public Sub Text(ByVal name As String)
+        If name = "" Then
+            Throw New ArgumentException()
+        End If
+
+    End Sub
+
+
+End Module
